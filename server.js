2387b1a986583308e188ae44ed3e37a056a2feb5
MoAlaa2 <moalaa2796@gmail.com>
2026-01-02 17:05:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok", service: "Guthmi API" });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log("API running on port", PORT);
 });
